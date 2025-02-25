fbcfdc82e0932ae629e847d8199045866cf019bb
Soubhagya <soubhagyaranjan1411@gmail.com>
2025-02-25 15:07:27 +0530

    Updated TypeScript files

--- TYPESCRIPT/index.js
+++ TYPESCRIPT/index.js
@@ -1,4 +1,21 @@
-"use strict";
-let a;
-a = 10;
-console.log(a);
+var user = {
+    username: "Soubhagya",
+    age: 23
+};
+console.log(user.username);
+user.age = 45;
+var num = [1, 2, 3, 4, 5];
+num[3] = 45;
+var str = ["soubhagya", "ranjan", "jena"];
+var numstr = [];
+numstr[0] = 1;
+numstr[1] = "10";
+var emp = {
+    id: 1,
+    ename: "Soubhagya",
+    salary: 100000,
+    dep: "IT",
+    getName: function () {
+        return this.ename;
+    }
+};
--- TYPESCRIPT/index.ts
+++ TYPESCRIPT/index.ts
@@ -1,4 +1,39 @@
-let a:number;
-a=10;
-console.log(a);
+interface User{
+    username:string;
+    age:number;
+}
+let user: User = {
+    username: "Soubhagya",
+    age: 23
+};
+console.log(user.username)
+user.age=45;
 
+let num:number[]=[1,2,3,4,5];
+
+num[3]=45;
+
+let str: string[] = ["soubhagya", "ranjan", "jena"];
+
+let numstr:number[] | string[] = [];
+numstr[0]=1;
+numstr[1]="10"
+//console.log(numstr[4]);
+
+interface Emp{
+    id:number,
+    ename:string,
+    salary:number,
+    dep:string,
+getName(): string;
+}
+
+let emp:Emp={
+    id:1,
+    ename:"Soubhagya",
+    salary:100000,
+    dep:"IT",
+    getName():string{
+        return this.ename;
+    }
+}
