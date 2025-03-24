adb66ec6edab7f5827d412e50906f3ac47caf911
Soubhagya <soubhagyaranjan1411@gmail.com>
2025-03-24 15:18:36 +0530

    Update code and folders

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,15 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
 
-function App() {
-  
+// const App = () => {
+//   let [cvalue, setCvalue] = useState(null);
 
+//   function getDataformChild(cdata) {
+//     console.log(cdata);
+//     setCvalue(cdata);
+//   }
+
+//   return (
+//     <div>
+//       <h1>App Component</h1>
+//       {cvalue && <h4>{cvalue}</h4>}
+//       <ChildComponent fn={getDataformChild}></ChildComponent>
+//     </div>
+//   );
+// }
+
+// function ChildComponent(props) {
+//   let fn = props.fn;
+//   let data = 10;
+//   return (
+//     <div>
+//       <h1>Child Component</h1>
+//       <button onClick={() => fn(data)}>Send Data</button>
+//     </div>
+//   );
+// }
+
+
+// export default App;
+
+import React, { useState } from 'react'
+
+const App = () => {
+  let [todos,setTodos]=useState([{id:"1",title:"Todo 1",description:"REACT + VITE"}])
+  return (
+    <div>
+      <h1>Todo application </h1>
+      <TodoInput></TodoInput>
+      <TodoList todos={todos}></TodoList>
+    </div>
+  )
+}
+
+function TodoInput(props){
   return(
-    <div>srj</div>
+    <form>
+      <input type="text" placeholder="Enter your todo title" />
+      <input type="text" placeholder="Enter your todo description" />
+      <button>Add Todo</button>
+    </form>
+  )
+}
+function TodoList(props){
+  let todos=props.todos;
+  return(
+    <div>
+    <ul>
+    {todos.map((todo)=>{
+return <TodoComponent key={todo.id} title={todo.title} description={todo.description} />
+    })}
+    </ul>
+    </div>
+  )
+
+}
+function TodoComponent(props){
+  return(
+    <li>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   )
 }
 
 export default App
- 
